44b4be4dac8f1ffcda263e07a10f5f9b3a5eca7b
chris <chrissouth89@gmail.com>
2020-08-12 19:31:33 -0500

    title fixes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <title>Learning Time</title>
         <Chatbot
           config={config}
           actionProvider={ActionProvider}
